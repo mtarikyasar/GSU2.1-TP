e20b58f164c3eea26598f987b8550acadca9be47
mtarikyasar <tarikyasar1@gmail.com>
2019-12-14 19:48:24 +0300

    initial commit.

---
+++ TP9/lib.h
@@ -0,0 +1,19 @@
+#ifndef lib_h
+#define lib_h
+
+int upperCase(char c);
+void ceaserEncoder(char *word, int shiftCount);
+void ceaserDecoder(char *word, int shiftCount);
+void vigenereEncoder(char *word, char *keyWord);
+void vigenereDecoder(char *word, char *keyWord);
+int sizeOfWord(char *word);
+void matrixCipherEncoder(char *word);
+void matrixCipherDecoder(char *word);
+void rsaAlgorithmEncoder(char* word, int p, int q);
+void rsaAlgorithmDecoder(char* word, int p, int q);
+int isPrime(int x);
+int reverseNumber(int x);
+int upCase(char c);
+int gcd(int a, int h);
+
+#endif
---
+++ TP9/main.c
@@ -0,0 +1,6 @@
+#include "lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+}
